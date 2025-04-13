5542715c694603be3a762bd40d5a394e0831ba8a
李蒙 <744816609@qq.com>
2025-04-13 14:38:52 +0800

    张韶涵阿刁 音轨号null

--- lib/mixin/song_control/parts/song_meta.dart
+++ lib/mixin/song_control/parts/song_meta.dart
@@ -12,92 +12,107 @@ class _SongMeta extends StatelessWidget {
         child: Column(
           children: [
             ListTile(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) =>
-                          ArtistDetailPage(artistId: song.artistId)));
-                },
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.person_2),
-                title: Text('歌手：${song.artist}')),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => ArtistDetailPage(artistId: song.artistId),
+                  ),
+                );
+              },
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.person_2),
+              title: Text('歌手：${song.artist}'),
+            ),
             const Divider(),
             ListTile(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => AlbumDetailPage(albumId: song.albumId)));
-                },
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.album_outlined),
-                title: Text('专辑：${song.album}')),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => AlbumDetailPage(albumId: song.albumId),
+                  ),
+                );
+              },
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.album_outlined),
+              title: Text('专辑：${song.album}'),
+            ),
             const Divider(),
             ListTile(
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.access_time),
-                title: Text('时长：${durationFormatter(song.duration)}')),
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.access_time),
+              title: Text('时长：${durationFormatter(song.duration)}'),
+            ),
             const Divider(),
             ListTile(
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.disc_full_outlined),
-                title: Text('格式：${song.suffix}')),
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.disc_full_outlined),
+              title: Text('格式：${song.suffix}'),
+            ),
             const Divider(),
             ListTile(
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.high_quality_outlined),
-                title: Text('比特率：${song.bitRate}K')),
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.high_quality_outlined),
+              title: Text('比特率：${song.bitRate}K'),
+            ),
             if (song.genre != null) const Divider(),
             if (song.genre != null)
               ListTile(
-                  dense: true,
-                  visualDensity:
-                      const VisualDensity(horizontal: 0, vertical: -4),
-                  leading: const Icon(Icons.gesture_rounded),
-                  title: Text('风格：${song.genre}')),
+                dense: true,
+                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+                leading: const Icon(Icons.gesture_rounded),
+                title: Text('风格：${song.genre}'),
+              ),
             if (song.year != null) const Divider(),
             if (song.year != null)
               ListTile(
-                  dense: true,
-                  visualDensity:
-                      const VisualDensity(horizontal: 0, vertical: -4),
-                  leading: const Icon(Icons.date_range_outlined),
-                  title: Text('年份：${song.year}')),
-            const Divider(),
-            ListTile(
                 dense: true,
                 visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.route_outlined),
-                title: Text('路径：${song.path}')),
+                leading: const Icon(Icons.date_range_outlined),
+                title: Text('年份：${song.year}'),
+              ),
+            const Divider(),
+            ListTile(
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.route_outlined),
+              title: Text('路径：${song.path}'),
+            ),
             if (song.samplingRate != null && song.samplingRate != 0)
               const Divider(),
             if (song.samplingRate != null && song.samplingRate != 0)
               ListTile(
-                  dense: true,
-                  visualDensity:
-                      const VisualDensity(horizontal: 0, vertical: -4),
-                  leading: const Icon(Icons.equalizer_outlined),
-                  title: Text('采样频率：${song.samplingRate}')),
-            const Divider(),
-            ListTile(
+                dense: true,
+                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+                leading: const Icon(Icons.equalizer_outlined),
+                title: Text('采样频率：${song.samplingRate}'),
+              ),
+            if (song.track != null) const Divider(),
+            if (song.track != null)
+              ListTile(
                 dense: true,
                 visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
                 leading: const Icon(Icons.disc_full_outlined),
-                title: Text('音轨号：${song.track}')),
+                title: Text('音轨号：${song.track}'),
+              ),
             const Divider(),
             ListTile(
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.pin_outlined),
-                title: Text('盘号：${song.bpm}')),
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.pin_outlined),
+              title: Text('盘号：${song.bpm}'),
+            ),
             const Divider(),
             ListTile(
-                dense: true,
-                visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
-                leading: const Icon(Icons.storage_rounded),
-                title: Text('文件大小：${fileSizeFormatter(song.size)}'))
+              dense: true,
+              visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+              leading: const Icon(Icons.storage_rounded),
+              title: Text('文件大小：${fileSizeFormatter(song.size)}'),
+            ),
           ],
         ),
       ),
