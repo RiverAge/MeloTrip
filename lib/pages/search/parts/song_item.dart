6d24f7d330a44bf033eebc20800b7b79c0dcc7c9
李蒙 <788816609@qq.com>
2025-04-13 14:21:47 +0800

    今日推荐的按下之后边距不对

--- lib/pages/search/parts/song_item.dart
+++ lib/pages/search/parts/song_item.dart
@@ -7,39 +7,50 @@ class _SongItem extends StatelessWidget with SongControl {
 
   @override
   Widget build(BuildContext context) => ListTile(
-      onTap: () async {
-        final navigator = Navigator.of(context);
-        final handler = await AppPlayerHandler.instance;
-        final player = handler.player;
-        await player.addSongToPlaylist(song, needPlay: true);
-        navigator.pop();
-      },
-      leading: Container(
-          height: 50,
-          width: 50,
-          clipBehavior: Clip.antiAlias,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5))),
-          child: ArtworkImage(id: song.id, fit: BoxFit.cover)),
-      title: Text('${song.title}',
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 3),
-              child: Row(children: [
-                _Tag(text: '${song.suffix}'),
-                _Tag(text: '${song.bitRate.toString()}k'),
-                _Tag(text: durationFormatter(song.duration ?? 0))
-              ])),
-          Text('《${song.album}》- ${song.artist}',
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontSize: 12))
-        ],
+    onTap: () async {
+      final navigator = Navigator.of(context);
+      final handler = await AppPlayerHandler.instance;
+      final player = handler.player;
+      await player.addSongToPlaylist(song, needPlay: true);
+      navigator.pop();
+    },
+    leading: Container(
+      height: 50,
+      width: 50,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
       ),
-      trailing: IconButton(
-          onPressed: () => showSongControlSheet(context, song.id),
-          icon: const Icon(Icons.more_horiz_rounded)));
+      child: ArtworkImage(id: song.id, fit: BoxFit.cover),
+    ),
+    title: Text(
+      '${song.title}',
+      overflow: TextOverflow.ellipsis,
+      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+    ),
+    subtitle: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3),
+          child: Row(
+            children: [
+              _Tag(text: '${song.suffix}'),
+              _Tag(text: '${song.bitRate.toString()}k'),
+              _Tag(text: durationFormatter(song.duration ?? 0)),
+            ],
+          ),
+        ),
+        Text(
+          '《${song.album}》- ${song.artist}',
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: 12),
+        ),
+      ],
+    ),
+    trailing: IconButton(
+      onPressed: () => showSongControlSheet(context, song.id),
+      icon: const Icon(Icons.more_horiz_rounded),
+    ),
+  );
 }
