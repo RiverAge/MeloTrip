06e4987b043576d5747be6b1f7c29b153b519ae8
李蒙 <744816609@qq.com>
2025-04-13 13:48:12 +0800

    搜索记录第一条的上边距不对

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -46,71 +46,95 @@ class _SearchPageState extends State<SearchPage> {
       child: Scaffold(
         appBar: AppBar(
           title: _Searchbar(
-              controller: _controller,
-              onFocusChange: (hasFocus) => setState(() {
-                    _isFoucs = hasFocus;
-                  }),
-              onSubmitted: (value) => setState(() {
-                    _query = value;
-                  })),
+            controller: _controller,
+            onFocusChange:
+                (hasFocus) => setState(() {
+                  _isFoucs = hasFocus;
+                }),
+            onSubmitted:
+                (value) => setState(() {
+                  _query = value;
+                }),
+          ),
         ),
-        body: _isFoucs
-            ? _SearchHistory(
-                onTap: (value) {
-                  _controller.text = value;
-                },
-              )
-            : _query == ''
+        body:
+            _isFoucs
+                ? _SearchHistory(
+                  onTap: (value) {
+                    _controller.text = value;
+                  },
+                )
+                : _query == ''
                 ? const SizedBox.shrink()
                 : AsyncValueBuilder(
-                    provider: searchProvider(_query),
-                    builder: (context, value, _) {
-                      final songs =
-                          value.subsonicResponse?.searchResult3?.song ?? [];
-                      final albums =
-                          value.subsonicResponse?.searchResult3?.album ?? [];
-                      final artists =
-                          value.subsonicResponse?.searchResult3?.artist ?? [];
-                      if (songs.isEmpty && albums.isEmpty && artists.isEmpty) {
-                        return const Padding(
-                          padding: EdgeInsets.only(top: 20.0),
-                          child: NoData(),
-                        );
-                      }
-                      return Column(
-                        children: [
-                          const TabBar(tabs: [
+                  provider: searchProvider(_query),
+                  builder: (context, value, _) {
+                    final songs =
+                        value.subsonicResponse?.searchResult3?.song ?? [];
+                    final albums =
+                        value.subsonicResponse?.searchResult3?.album ?? [];
+                    final artists =
+                        value.subsonicResponse?.searchResult3?.artist ?? [];
+                    if (songs.isEmpty && albums.isEmpty && artists.isEmpty) {
+                      return const Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: NoData(),
+                      );
+                    }
+                    return Column(
+                      children: [
+                        const TabBar(
+                          tabs: [
                             Tab(text: '歌曲'),
                             Tab(text: '专辑'),
-                            Tab(text: '艺术家')
-                          ]),
-                          Expanded(
-                            child: TabBarView(
-                              children: [
-                                ListView.separated(
-                                    itemBuilder: (_, index) =>
-                                        _SongItem(song: songs[index]),
-                                    separatorBuilder: (_, index) =>
-                                        const Divider(),
-                                    itemCount: songs.length),
-                                ListView.separated(
-                                    itemBuilder: (_, index) =>
+                            Tab(text: '艺术家'),
+                          ],
+                        ),
+                        Expanded(
+                          child: TabBarView(
+                            children: [
+                              ListView.separated(
+                                itemBuilder:
+                                    (_, index) => _SongItem(song: songs[index]),
+                                separatorBuilder:
+                                    (_, index) => const Divider(
+                                      indent: 0,
+                                      endIndent: 0,
+                                      height: 0,
+                                    ),
+                                itemCount: songs.length,
+                              ),
+                              ListView.separated(
+                                itemBuilder:
+                                    (_, index) =>
                                         _AlbumItem(album: albums[index]),
-                                    separatorBuilder: (_, index) =>
-                                        const Divider(),
-                                    itemCount: albums.length),
-                                ListView.separated(
-                                    itemBuilder: (_, index) =>
+                                separatorBuilder:
+                                    (_, index) => const Divider(
+                                      indent: 0,
+                                      endIndent: 0,
+                                      height: 0,
+                                    ),
+                                itemCount: albums.length,
+                              ),
+                              ListView.separated(
+                                itemBuilder:
+                                    (_, index) =>
                                         _ArtistItem(artist: artists[index]),
-                                    separatorBuilder: (_, index) =>
-                                        const Divider(),
-                                    itemCount: artists.length),
-                              ],
-                            ),
+                                separatorBuilder:
+                                    (_, index) => const Divider(
+                                      indent: 0,
+                                      endIndent: 0,
+                                      height: 0,
+                                    ),
+                                itemCount: artists.length,
+                              ),
+                            ],
                           ),
-                        ],
-                      );
-                    }),
+                        ),
+                      ],
+                    );
+                  },
+                ),
       ),
     );
   }
