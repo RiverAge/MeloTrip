6c6d4bf701a27c5c089678ef06fda4938586b50c
李蒙 <788816609@qq.com>
2025-04-18 16:54:00 +0800

    优化搜索页面 window上可以点击历史词条显示了 点击历史词条可以直接搜索了 添加了清楚按钮

--- lib/pages/search/parts/search_bar.dart
+++ lib/pages/search/parts/search_bar.dart
@@ -1,12 +1,13 @@
 part of "../search_page.dart";
 
 class _Searchbar extends StatefulWidget {
-  const _Searchbar(
-      {required this.onSubmitted,
-      required this.controller,
-      required this.onFocusChange});
+  const _Searchbar({
+    required this.onSubmitted,
+    required this.controller,
+    required this.onReFocused,
+  });
   final void Function(String) onSubmitted;
-  final void Function(bool hasFocus) onFocusChange;
+  final void Function() onReFocused;
   final TextEditingController controller;
 
   @override
@@ -18,47 +19,68 @@ class _SearchbarState extends State<_Searchbar> {
 
   @override
   void initState() {
-    _focusNode.addListener(_onFocusChange);
-    _focusNode.requestFocus();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _focusNode.requestFocus();
+    });
     super.initState();
   }
 
   @override
   void dispose() {
-    _focusNode.removeListener(_onFocusChange);
     _focusNode.dispose();
 
     super.dispose();
   }
 
-  _onFocusChange() {
-    widget.onFocusChange(_focusNode.hasFocus);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
       decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-          border: Border.all(
-              width: 1, color: Theme.of(context).colorScheme.primary)),
-      child: TextField(
-          controller: widget.controller,
-          onSubmitted: (val) {
-            _focusNode.unfocus();
-            widget.onSubmitted(val);
-          },
-          focusNode: _focusNode,
-          cursorHeight: 17,
-          textInputAction: TextInputAction.search,
-          decoration: const InputDecoration(
-              isDense: true,
-              icon: Icon(Icons.search),
-              hintText: '搜索专辑、艺术家、歌曲',
-              border: InputBorder.none)),
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.search, size: 19),
+          SizedBox(width: 5),
+          Expanded(
+            child: TextField(
+              controller: widget.controller,
+              style: TextStyle(fontSize: 13),
+              onSubmitted: (val) {
+                _focusNode.unfocus();
+                widget.onSubmitted(val);
+              },
+              focusNode: _focusNode,
+              cursorHeight: 17,
+              textInputAction: TextInputAction.search,
+              decoration: const InputDecoration(
+                isDense: true,
+                // icon: Icon(Icons.search),
+                hintText: '搜索专辑、艺术家、歌曲',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          IconButton(
+            constraints: BoxConstraints(),
+            padding: EdgeInsets.all(2),
+            onPressed: () {
+              widget.controller.text = '';
+              _focusNode.requestFocus();
+              widget.onReFocused();
+            },
+            icon: Icon(Icons.clear, size: 19),
+          ),
+        ],
+      ),
     );
   }
+
   // @override
   // Widget build(BuildContext context) => Container(
   //       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
--- lib/pages/search/parts/search_history.dart
+++ lib/pages/search/parts/search_history.dart
@@ -17,47 +17,60 @@ class _SearchHistory extends StatelessWidget {
               children: [
                 const Text("搜索历史"),
                 // Icon(Icons.delete)
-                Consumer(builder: (context, ref, child) {
-                  return IconButton(
+                Consumer(
+                  builder: (context, ref, child) {
+                    return IconButton(
                       onPressed: () {
                         ref.read(searchHistoryProvider.notifier).clearAll();
                       },
-                      icon: const Icon(Icons.delete));
-                })
+                      icon: const Icon(Icons.delete),
+                    );
+                  },
+                ),
               ],
             ),
             SizedBox(
-                width: double.infinity,
-                child: AsyncValueBuilder(
-                    provider: searchHistoryProvider,
-                    empty: (context, ref) => const SizedBox.shrink(),
-                    builder: (context, data, ref) {
-                      return Wrap(
-                          spacing: 10.0,
-                          runSpacing: 5.0,
-                          alignment: WrapAlignment.start,
-                          runAlignment: WrapAlignment.center,
-                          children: data
-                              .map((e) => InkWell(
-                                  onTap: () {
-                                    onTap(e);
-                                  },
-                                  child: Container(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 5, horizontal: 10),
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurface
-                                          .withValues(alpha: 0.1),
-                                      child: Text(
-                                        e,
-                                        style: TextStyle(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onSurfaceVariant),
-                                      ))))
-                              .toList());
-                    }))
+              width: double.infinity,
+              child: AsyncValueBuilder(
+                provider: searchHistoryProvider,
+                empty: (context, ref) => const SizedBox.shrink(),
+                builder: (context, data, ref) {
+                  return Wrap(
+                    spacing: 10.0,
+                    runSpacing: 5.0,
+                    alignment: WrapAlignment.start,
+                    runAlignment: WrapAlignment.center,
+                    children:
+                        data
+                            .map(
+                              (e) => InkWell(
+                                onTap: () {
+                                  onTap(e);
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 5,
+                                    horizontal: 10,
+                                  ),
+                                  color: Theme.of(context).colorScheme.onSurface
+                                      .withValues(alpha: 0.1),
+                                  child: Text(
+                                    e,
+                                    style: TextStyle(
+                                      color:
+                                          Theme.of(
+                                            context,
+                                          ).colorScheme.onSurfaceVariant,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
