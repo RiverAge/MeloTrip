a05de1506b9afdf78223348db3fca81cbf354828
李蒙 <744816609@qq.com>
2025-04-30 09:35:17 +0800

    头像优化显示

--- lib/pages/search/parts/artist_item.dart
+++ lib/pages/search/parts/artist_item.dart
@@ -7,17 +7,23 @@ class _ArtistItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ListTile(
-      leading: Container(
-          height: 50,
-          width: 50,
-          clipBehavior: Clip.antiAlias,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5))),
-          child: ArtworkImage(id: artist.coverArt)),
-      title: Text(artist.name ?? ''),
-      subtitle: Text('专辑数量 ${artist.albumCount}'),
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(
-            builder: (_) => ArtistDetailPage(artistId: artist.id)));
-      });
+    leading: Container(
+      height: 50,
+      width: 50,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: ArtworkImage(id: artist.coverArt, fit: BoxFit.cover),
+    ),
+    title: Text(artist.name ?? ''),
+    subtitle: Text('专辑数量 ${artist.albumCount}'),
+    onTap: () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => ArtistDetailPage(artistId: artist.id),
+        ),
+      );
+    },
+  );
 }
