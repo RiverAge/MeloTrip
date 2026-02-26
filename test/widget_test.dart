4afdd5a49efdb27c1801323a94afbb408b23291f
riverage <744816609@qq.com>
2026-02-26 17:59:13 +0800

    test: add initial page login-route coverage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:melo_trip/app_player/player.dart';
 import 'package:melo_trip/l10n/app_localizations.dart';
+import 'package:melo_trip/model/auth_user/auth_user.dart';
 import 'package:melo_trip/pages/home/home_page.dart';
 import 'package:melo_trip/provider/album/albums.dart';
+import 'package:melo_trip/pages/initial/initial_page.dart';
+import 'package:melo_trip/pages/login/login_page.dart';
+import 'package:melo_trip/provider/app_player/app_player.dart';
+import 'package:melo_trip/provider/auth/auth.dart';
 import 'package:melo_trip/provider/route/route_observer.dart';
 
 void main() {
@@ -57,4 +63,36 @@ void main() {
     final observer = container.read(routeObserverProvider);
     expect(observer, isA<RouteObserver<ModalRoute<void>>>());
   });
+
+  testWidgets('InitialPage routes to LoginPage when user is not logged in', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          appPlayerHandlerProvider.overrideWith(_FakeAppPlayerHandler.new),
+          currentUserProvider.overrideWith(_FakeCurrentUser.new),
+        ],
+        child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: const InitialPage(),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(LoginPage), findsOneWidget);
+  });
+}
+
+class _FakeAppPlayerHandler extends AppPlayerHandler {
+  @override
+  Future<AppPlayer?> build() async => null;
+}
+
+class _FakeCurrentUser extends CurrentUser {
+  @override
+  Future<AuthUser?> build() async => null;
 }
