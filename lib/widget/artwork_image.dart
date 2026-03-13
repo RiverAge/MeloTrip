ce5d6e3ce6cd48aa3b81432c4da48144f0a294ff
riverage <744816609@qq.com>
2026-03-13 12:53:51 +0800

    Fix artwork loading on web

--- lib/widget/artwork_image.dart
+++ lib/widget/artwork_image.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:melo_trip/const/index.dart';
@@ -35,7 +36,13 @@ class ArtworkImage extends ConsumerWidget {
 
     return switch (auth) {
       AsyncData(:final value) => Image.network(
-        '$proxyCacheHost/rest/getCoverArt?id=$artworkId&u=${value?.username}&t=${value?.token}&s=${value?.salt}&f=json&v=1.8.0&c=MeloTrip&size=${size ?? 500}',
+        _buildArtworkUrl(
+          artworkId: artworkId,
+          host: value?.host,
+          username: value?.username,
+          token: value?.token,
+          salt: value?.salt,
+        ),
         width: width,
         height: height,
         fit: fit,
@@ -81,5 +88,34 @@ class ArtworkImage extends ConsumerWidget {
         ),
       ),
     );
+  }
+
+  String _buildArtworkUrl({
+    required String artworkId,
+    required String? host,
+    required String? username,
+    required String? token,
+    required String? salt,
+  }) {
+    final baseHost = kIsWeb ? host : proxyCacheHost;
+    final normalizedBase =
+        (baseHost == null || baseHost.isEmpty)
+            ? proxyCacheHost
+            : baseHost.endsWith('/')
+            ? baseHost.substring(0, baseHost.length - 1)
+            : baseHost;
+    final query = <String, String>{
+      'id': artworkId,
+      'f': 'json',
+      'v': '1.8.0',
+      'c': 'MeloTrip',
+      'size': '${size ?? 500}',
+      if (username != null && username.isNotEmpty) 'u': username,
+      if (token != null && token.isNotEmpty) 't': token,
+      if (salt != null && salt.isNotEmpty) 's': salt,
+    };
+    return Uri.parse('$normalizedBase/rest/getCoverArt').replace(
+      queryParameters: query,
+    ).toString();
   }
 }
