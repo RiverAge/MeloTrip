191bb3478d4bab7e248b3a37b76d277f502c7641
riverage <744816609@qq.com>
2026-03-13 11:48:23 +0800

    Remove obsolete provider entrypoints

--- lib/provider/route/route_observer.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final routeObserverProvider = Provider<RouteObserver<PageRoute<dynamic>>>(
-  (_) => RouteObserver<PageRoute<dynamic>>(),
-);
